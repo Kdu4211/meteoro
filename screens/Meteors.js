7bcec2b0abd8ba7435633be9ce397d9d245d6d9a
Thays Silva <113531216+Profthayssilva@users.noreply.github.com>
2023-12-25 22:17:47 -0300

    Update Meteors.js

--- screens/Meteors.js
+++ screens/Meteors.js
@@ -1,14 +1,15 @@
 import React, {Component} from "react";
 import {Text, View} from "react-native";
-export default class MeteorsScreen extends Component{
+
+export default class MeteorScreen extends Component{
     render(){
         return(
-            <View style ={{
+            <View style={{
                 flex: 1,
                 justifyContent: "center",
                 alignItems: "center"
             }}>
-                <Text>Tela de Meteoro</Text>
+                <Text>Tela de Meteoros</Text>
             </View>
         )
     }
